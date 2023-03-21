e6b9de38ed4554b6f476897153910c98eff2632b
Azwa09 <azwaaleeya09@gmail.com>
2023-03-21 15:12:00 +0800

    new

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
-# Welcome to GitHub Desktop!
+<!DOCTYPE html>
+<html lang="en">
 
-This is your README. READMEs are where you can communicate what your project is and how to use it.
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise</title>
+</head>
 
-Write your name on line 6, save it, and then head back to GitHub Desktop.
+<body>
+    <h3>SIFIR</h3>
+    <form method="POST">
+        <label for="num">Nombor sifir:</label>
+        <input type="number" name="num">
+        <button type="submit" name="kiraan">Kiraan</button>
+        <p></p>
+    </form>
+
+    <?php
+    if (isset($_POST['kiraan'])) {
+        $num = intval($_POST['num']);
+        if ($num >= 1 && $num <= 20) {
+          echo "<table>";
+          for ($i = 1; $i <= 20; $i++) {
+            $result = $i * $num;
+            echo "<tr><td>{$i} x {$num}</td><td> = {$result}</td></tr>";
+          }
+          echo "</table>";
+        } 
+      }
+    ?>
+</body>
+
+</html>
